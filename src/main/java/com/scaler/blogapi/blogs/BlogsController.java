1d31cb3034b939f111439a442bc4a307120b3d5b
srinivasa2.p <srinivasa2.p@ril.com>
2023-07-31 22:34:35 +0530

    initialize

---
+++ src/main/java/com/scaler/blogapi/blogs/BlogsController.java
@@ -0,0 +1,9 @@
+package com.scaler.blogapi.blogs;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/blogs")
+public class BlogsController {
+}
